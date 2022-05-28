f71c03316996c5f98ddfc50e5da43ec2fc15bf99
ejc <ejcarlson787@outlook.com>
2022-05-28 02:05:25 -0400

    Editing make_release.sh and adding generate_release_tag.sh
    Issue #22  - Removing the release tag from make_release.sh and adding to generate_release_tag.sh because I forgot to commit changes before making the release. I may have to delete the v0.1.26 release so I can make it again.

---
+++ build_scripts/generate_release_tag.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#Generate GitHub release tag
+#2022-ejc
+#
+##################################################
+#Variables
+##################################################
+version=$(grep Version ../trunk/buildroot-2021.02.1/board/stmicroelectronics/stm32mp157a-dk1/overlay/etc/motd | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/')
+echo ""
+echo "*******************************************************"
+echo "Don't run this until all sources are committed to main!"
+echo "Are you sure you want to generate release tag?"
+echo "*******************************************************"
+echo "You will also need your GitHub personal access token!"
+echo "*******************************************************"
+read -n1 -r -p "Press any non-NUL character to continue...CTRL+C to exit..." key
+##################################################
+#Generate release tag...
+##################################################
+git tag -s v$version -m "stm32_$version"
+git tag --verify 
+git push --tags origin
+echo "Done!"
